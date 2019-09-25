918d7c830abe53cd06aafa15284ac4f54930e84f
Turar Abu <mail@turarabu.me>
2019-09-25 15:06:01 +0600

    auto-updater

---
+++ core/update.js
@@ -0,0 +1,59 @@
+const { autoUpdater } = require('electron-updater')
+const { app } = require('electron')
+
+const Window = require('./window')
+const ipc = require('./ipc')
+
+const temp = []
+const wName = 'update'
+
+module.exports = { check, start, windowOpen }
+
+function check () {
+    ipc.send('set-update-status', wName, 1)
+
+    return new Promise(function (resolve) {
+        autoUpdater.autoDownload = false
+        autoUpdater.checkForUpdates()
+            .then(result => {
+                var available = result.updateInfo.version
+                resolve(available > app.getVersion())
+            })
+            .catch(() => {
+                setTimeout(async () => {
+                    temp[0] = await check()
+                    resolve(temp[0])
+                }, 10 * 1000)
+            })
+    })
+}
+
+async function start () {
+    ipc.send('set-update-status', wName, 2)
+
+    return new Promise(function (resolve) {
+        autoUpdater.downloadUpdate()
+            .then(result => {
+                if( result.length > 0 ) {
+                    ipc.send('set-update-status', wName, 3)
+                    autoUpdater.quitAndInstall(true, true)
+                }
+            })
+            .catch(() => {
+                setTimeout(async () => {
+                    temp[1] = await start()
+                    resolve(temp[1])
+                }, 10 * 1000)
+            })
+    })
+    // app.exit()
+}
+
+function windowOpen () {
+    Window.open(wName, {
+        height: 320,
+        width: 260
+    }).then(window => {
+        window.webContents.openDevTools()
+    })
+}
---
+++ source/update.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+
+body#update
+    h3( class='title center' ) AniLibria
+    p( class='status center' ) {{ status.text[ status.code ] }}
+    img( class='loading center' src='~src/assets/images/loading.gif' )
+
+    img( class='version-sticker' src='~src/assets/images/Nezuko-01.png' )
+    div( class='version-div' )
+        span( class='text' ) Version: 0.2.0
+        span( class='text' ) Nezuko
+
+</template>
+
+<script>
+export default {
+    mounted: start,
+    data: function () {
+        return {
+            status: {
+                text: ['Подготовка', 'Проверка наличия обновлении', 'Скачиваем обновления', 'Установка'],
+                code: 1
+            }
+        }
+    }
+}
+
+function start () {
+    ipc.on('set-update-status', (sender, codess) => {
+        this.status.code = code
+    })
+}
+</script>
+
+<style lang="stylus">
+@import '~src/style/base'
+@import '~src/style/palette'
+
+#update
+    background $midnight
+    color $white
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom 0
+    
+    .center
+        display block
+        margin 0 auto
+        text-align center
+
+    .title
+        color $red
+        font-size 52px
+        font-weight 400
+        margin-top 32px
+
+    .status
+        color darken($white, 5)
+        font-size 14px
+        font-weight 300
+        margin-top 18px
+
+    .loading
+        height 8px
+        margin-top 6px
+        position relative
+        left -10px
+
+    .version-sticker
+        position absolute
+        bottom 2px
+        right 6px
+        max-height 128px
+        max-width 128px
+
+    .version-div
+        position absolute
+        bottom 12px
+        left 14px
+
+        .text
+            color darken($white, 15)
+            display block
+            font-size 10px
+            font-weight 300
+            line-height 11px
+
+</style>
